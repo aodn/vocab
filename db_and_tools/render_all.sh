2af904d1c92c16f89cf585ffd6f9ec6ce2bc9480
David Nahodil <dnahodil@utas.edu.au>
2015-04-30 16:33:01 +1000

    Pass arguments to render.rb

--- db_and_tools/render_all.sh
+++ db_and_tools/render_all.sh
@@ -5,12 +5,12 @@ rm -rf "$output"
 mkdir "$output"
 
 
-./render.rb -t AODNParameterVocabulary.erb			> $output/AODNParameterVocabulary.xml
-./render.rb -t parameterClassificationScheme.erb	> $output/parameterClassificationScheme.xml
+./render.rb -t AODNParameterVocabulary.erb $@        > $output/AODNParameterVocabulary.xml
+./render.rb -t parameterClassificationScheme.erb $@  > $output/parameterClassificationScheme.xml
 
 
-./render.rb -t AODNPlatformVocabulary.erb			> $output/AODNPlatformVocabulary.xml
-./render.rb -t platformClassificationScheme.erb		> $output/platformClassificationScheme.xml
+./render.rb -t AODNPlatformVocabulary.erb $@         > $output/AODNPlatformVocabulary.xml
+./render.rb -t platformClassificationScheme.erb $@   > $output/platformClassificationScheme.xml
 
 find -type f -size 0b -iname "*" -exec rm {} \;
 
